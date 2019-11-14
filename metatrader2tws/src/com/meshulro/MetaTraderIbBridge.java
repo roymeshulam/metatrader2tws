f119ebc531f6c7738a8f9188a41adbdfb26a40e5
royme <royme@DESKTOP-996L493>
2019-11-14 13:45:08 +0100

    Updated to support GTC limit orders

--- metatrader2tws/src/com/meshulro/MetaTraderIbBridge.java
+++ metatrader2tws/src/com/meshulro/MetaTraderIbBridge.java
@@ -10,7 +10,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.text.SimpleDateFormat;
 import java.time.ZonedDateTime;
 import java.util.Properties;
 import java.util.concurrent.Executors;
@@ -94,112 +93,127 @@ public class MetaTraderIbBridge {
 			System.exit(1);
 		} else {
 			m_logger.info("Initialization complete, connected MetaTrader to IB Account ".concat(m_wrapper.account()));
-			m_logger.info("Order book last modified date "
-					+ new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(m_orderBookLastModified));
 		}
 	}
 
 	public void CheckForNewTradingInstructions() {
-		final Runnable orderBookReader = new Runnable() {
-			@Override
-			public void run() {
-				final File file = new File(m_orderBookFilePath);
-				if (file.lastModified() != 0 && file.lastModified() != m_orderBookLastModified) {
-					m_orderBookLastModified = file.lastModified();
+		final Runnable orderBookReader = () -> {
+			final File file = new File(m_orderBookFilePath);
+			if (file.lastModified() != 0 && file.lastModified() != m_orderBookLastModified) {
+				m_orderBookLastModified = file.lastModified();
 
-					MetaTraderContract l_metaTraderContract = null;
-					try {
-						final BufferedReader br = new BufferedReader(new FileReader(file));
-						l_metaTraderContract = MetaTraderContract.FromString(br.readLine());
-						br.close();
-					} catch (final FileNotFoundException e) {
-						m_logger.severe("Order book file wasn't found, returning");
-						m_logger.severe(e.toString());
-						return;
-					} catch (final IOException e) {
-						m_logger.severe("Failed reading order book file, returning");
-						m_logger.severe(e.toString());
-						return;
-					}
-
-					m_logger.info("New instruction");
-					m_logger.info(l_metaTraderContract.toString());
-
-					final EClientSocket l_client = m_wrapper.getClient();
-					final EReaderSignal l_signal = m_wrapper.getSignal();
-					l_client.eConnect("127.0.0.1", 4002, 0);
-
-					final EReader l_reader = new EReader(l_client, l_signal);
-					l_reader.start();
-					new Thread() {
-						@Override
-						public void run() {
-							while (l_client.isConnected()) {
-								l_signal.waitForSignal();
-								try {
-									l_reader.processMsgs();
-								} catch (final IOException e) {
-									m_logger.severe("Reader encountered IO Exception");
-									m_logger.severe(e.toString());
-								}
-
-							}
-						}
-					}.start();
-
-					try {
-						Thread.sleep(1000);
-					} catch (final InterruptedException e) {
-						m_logger.info("Thread encountered InterruptedException");
-						m_logger.info(e.toString());
-					}
-
-					if (l_metaTraderContract.action().equals("Close")) {
-					} else {
-						final double l_totalQuantity = Math
-								.round(l_metaTraderContract.m_relativeSize * getAvailableFunds());
-						if (l_totalQuantity > 0) {
-							final Contract l_marketContract = new Contract();
-							l_marketContract.symbol(l_metaTraderContract.currency1());
-							l_marketContract.secType("CASH");
-							l_marketContract.currency(l_metaTraderContract.currency2());
-							l_marketContract.exchange("IDEALPRO");
-
-							final Order l_marketOrder = new Order();
-							l_marketOrder.action(l_metaTraderContract.action());
-							l_marketOrder.orderType("MKT");
-							l_marketOrder.totalQuantity(l_totalQuantity);
-
-							l_client.reqIds(-1);
-							l_client.placeOrder(m_wrapper.getCurrentOrderId() + 1, l_marketContract, l_marketOrder);
-
-							if (l_metaTraderContract.m_takeProfit > 0) {
-								final Contract l_limitContract = new Contract();
-								l_limitContract.symbol(l_metaTraderContract.currency1());
-								l_limitContract.secType("CASH");
-								l_limitContract.currency(l_metaTraderContract.currency2());
-								l_limitContract.exchange("IDEALPRO");
-
-								final Order l_limitOrder = new Order();
-								l_limitOrder.action(l_metaTraderContract.action() == "Buy" ? "Sell" : "Buy");
-								l_limitOrder.orderType("LMT");
-								l_limitOrder.totalQuantity(l_totalQuantity);
-								l_limitOrder.lmtPrice(l_metaTraderContract.takepProfit());
-
-								l_client.placeOrder(m_wrapper.getCurrentOrderId() + 1, l_limitContract, l_limitOrder);
-							}
-						} else {
-							m_logger.severe("getAvailableFunds returned 0, please place order manually");
-						}
-					}
-					try {
-						Thread.sleep(60000);
-					} catch (final InterruptedException e) {
-						m_logger.info("Thread encountered InterruptedException");
-						m_logger.info(e.toString());
-					}
-					l_client.eDisconnect();
+				MetaTraderContract l_metaTraderContract = null;
+				try {
+					final BufferedReader br = new BufferedReader(new FileReader(file));
+					l_metaTraderContract = MetaTraderContract.FromString(br.readLine());
+					br.close();
+				} catch (final FileNotFoundException e1) {
+					m_logger.severe("Order book file wasn't found, returning");
+					m_logger.severe(e1.toString());
+					return;
+				} catch (final IOException e2) {
+					m_logger.severe("Failed reading order book file, returning");
+					m_logger.severe(e2.toString());
+					return;
 				}
+
+				m_logger.info("New instruction");
+				m_logger.info(l_metaTraderContract.toString());
+
+				final EClientSocket l_client = m_wrapper.getClient();
+				final EReaderSignal l_signal = m_wrapper.getSignal();
+				l_client.eConnect("127.0.0.1", 4002, 0);
+
+				final EReader l_reader = new EReader(l_client, l_signal);
+				l_reader.start();
+				new Thread() {
+					@Override
+					public void run() {
+						while (l_client.isConnected()) {
+							l_signal.waitForSignal();
+							try {
+								l_reader.processMsgs();
+							} catch (final IOException e) {
+								m_logger.severe("Reader encountered IO Exception");
+								m_logger.severe(e.toString());
+							}
+
+						}
+					}
+				}.start();
+
+				try {
+					Thread.sleep(2000);
+				} catch (final InterruptedException e3) {
+					m_logger.info("Thread encountered InterruptedException");
+					m_logger.info(e3.toString());
+				}
+
+				if (l_metaTraderContract.action().equals("Close")) {
+				} else {
+					final double l_totalQuantity = Math
+							.round(l_metaTraderContract.m_relativeSize * getAvailableFunds());
+					if (l_totalQuantity > 0) {
+						int l_currentOrderId = m_wrapper.getCurrentOrderId();
+
+						final Contract l_marketContract = new Contract();
+						l_marketContract.secType("CFD");
+						l_marketContract.exchange("SMART");
+						l_marketContract.symbol(l_metaTraderContract.currency1());
+						l_marketContract.currency(l_metaTraderContract.currency2());
+
+						final Order l_marketOrder = new Order();
+						l_marketOrder.action(l_metaTraderContract.action());
+						l_marketOrder.orderType("MKT");
+						l_marketOrder.totalQuantity(l_totalQuantity);
+
+						m_logger.info(
+								"Placing market order " + l_marketContract.toString() + " " + l_marketOrder.toString());
+						l_client.placeOrder(l_currentOrderId++, l_marketContract, l_marketOrder);
+
+						try {
+							Thread.sleep(10000);
+						} catch (final InterruptedException e4) {
+							m_logger.info("Thread encountered InterruptedException");
+							m_logger.info(e4.toString());
+						}
+
+						if (l_metaTraderContract.m_takeProfit > 0) {
+							final Contract l_limitContract = new Contract();
+							l_limitContract.secType("CFD");
+							l_limitContract.exchange("SMART");
+							l_limitContract.symbol(l_metaTraderContract.currency1());
+							l_limitContract.currency(l_metaTraderContract.currency2());
+
+							final Order l_limitOrder = new Order();
+							l_limitOrder.action(l_metaTraderContract.action() == "Buy" ? "Sell" : "Buy");
+							l_limitOrder.tif("GTC");
+							l_limitOrder.orderType("LMT");
+							l_limitOrder.totalQuantity(l_totalQuantity);
+							l_limitOrder.lmtPrice(l_metaTraderContract.takepProfit());
+
+							m_logger.info("Placing limit order " + l_limitContract.toString() + " "
+									+ l_limitOrder.toString());
+							l_client.placeOrder(l_currentOrderId++, l_limitContract, l_limitOrder);
+
+							try {
+								Thread.sleep(10000);
+							} catch (final InterruptedException e5) {
+								m_logger.info("Thread encountered InterruptedException");
+								m_logger.info(e5.toString());
+							}
+						}
+					} else {
+						m_logger.severe("getAvailableFunds returned 0, please place order manually");
+					}
+				}
+				try {
+					Thread.sleep(60000);
+				} catch (final InterruptedException e6) {
+					m_logger.info("Thread encountered InterruptedException");
+					m_logger.info(e6.toString());
+				}
+				l_client.eDisconnect();
 			}
 		};
 
